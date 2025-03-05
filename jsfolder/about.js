2da4b149f43cb3a9cbf3036c91e43b0dc51e4b8e
nahidkhan31 <nahidkhanniloy31@.com>
2025-03-05 17:13:18 +0600

    added about section

---
+++ jsfolder/about.js
@@ -0,0 +1,32 @@
+document.getElementById('about').addEventListener('click', function(event){
+ event.preventDefault;
+  window.location.href="about.html"
+})
+
+document.getElementById('about2').addEventListener('click', function(event){
+    event.preventDefault;
+     window.location.href="about.html"
+   })
+
+   document.getElementById('about3').addEventListener('click', function(event){
+    event.preventDefault;
+     window.location.href="about.html"
+   })
+
+//    back home
+document.getElementById('home').addEventListener('click', function(event){
+    event.preventDefault;
+     window.location.href="index.html"
+   })
+
+   document.getElementById('home2').addEventListener('click', function(event){
+    event.preventDefault;
+     window.location.href="index.html"
+   })
+
+   document.getElementById('home3').addEventListener('click', function(event){
+    event.preventDefault;
+     window.location.href="index.html"
+   })
+
+
